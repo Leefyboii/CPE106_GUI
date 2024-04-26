4d44d141f6525f7a1da04cf8964cd1d460bae432
Leefyboii <148671922+Leefyboii@users.noreply.github.com>
2024-04-27 03:15:52 +0800

    Update Login.xaml.cs

--- CPE106_GUI/Login.xaml.cs
+++ CPE106_GUI/Login.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,6 +24,69 @@ namespace CPE106_GUI
             InitializeComponent();
 
         }
+
+        private void ExitButtonLogin_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to exit?", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void ExitButtonRegis_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to exit?", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Login Code Here
+
+            bool isAuthenticated = true; 
+
+            if (isAuthenticated)
+            {
+                MessageBox.Show("Login Successful", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                this.Close();
+
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                MessageBox.Show("Login Failed. Please try again.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Registration code here
+
+            bool isRegistered = true; 
+
+            if (isRegistered)
+            {
+                MessageBox.Show("Registration Successful\nPlease Login", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                if (MyTabControl != null && MyTabControl.Items.Count > 0)
+                {
+                    MyTabControl.SelectedIndex = 0;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Registration Failed. Please try again.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
 
         private void EmailTextBoxLogin_TextChanged(object sender, TextChangedEventArgs e)
         {
